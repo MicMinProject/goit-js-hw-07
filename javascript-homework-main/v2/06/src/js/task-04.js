b1da6b51a3c29c7553925916f4d80f3fd68e7305
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2021-08-28 17:26:42 +0200

    Update task-04.js

--- javascript-homework-main/v2/06/src/js/task-04.js
+++ javascript-homework-main/v2/06/src/js/task-04.js
@@ -2,14 +2,18 @@ let counterValue=0;
 
 const decrement=document.querySelector('#counter').firstElementChild;
 const increment=document.querySelector('#counter').lastElementChild;
-let value=document.querySelector('#value');
-const firstCallback = () => {
-  console.log("First callback!");
-};
-const increase=()=>{
-  counterValue+=1;
-  value.textContent=counterValue};
-const decrease=()=>{counterValue-=1;value.textContent=counterValue}
+let span=document.querySelector('#value');
+
+const increase=(e)=>{
+valueCounter+=Number(increment.textContent);
+span.textContent=valueCounter;
+console.log(span.textContent)
+}
+const decrease=(e)=>{
+valueCounter+=Number(decrement.textContent);
+span.textContent=valueCounter;
+console.log(span.textContent)
+}
 
 decrement.addEventListener('click',decrease);
 increment.addEventListener('click',increase);
