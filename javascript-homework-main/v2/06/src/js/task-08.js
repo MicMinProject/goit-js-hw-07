7e2eae429ffc1ecf3dbc3cfd60f2192a3f6146c0
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2021-08-28 20:23:28 +0200

    Update task-08.js

--- javascript-homework-main/v2/06/src/js/task-08.js
+++ javascript-homework-main/v2/06/src/js/task-08.js
@@ -5,20 +5,22 @@ const password=document.querySelector('input[type="password"]');
 const submitFc=(e)=>{
 e.preventDefault();
 
-const{elements:{email,password}}=e.currentTarget;
+const{elements: {email,password}}=e.currentTarget;
 
-let dataEmail=email.value;let dataPassword=password.value;
+if(email.value===""||password.value===""){
+alert('Fill up all the poles, please')
+}else{
+ const firstProperty=email.getAttribute('name');
+ const secondProperty=password.getAttribute('name');
+  
+  const user={};
 
-if(dataEmail==='' || dataPassword===''){
-return alert(`Fill up all the poles, please`)}
-else{
-  const data={
-    email: dataEmail,
-    password: dataPassword,
-  }
-  console.log(data)
-  return form.reset()
+  user[firstProperty]=email.value;
+  user[secondProperty]=password.value
+  console.log(user)
+}
+e.currentTarget.reset()
 }
 
-}
+
 form.addEventListener('submit',submitFc)
